ab2880def98c5fe5f34ecaf5ed1933cb3d6f9634
jProgr <8537691@gmail.com>
2020-02-23 11:09:29 -0600

    Add Ingredient

---
+++ src/test/java/com/jprogr/taco/Ingredient.java
@@ -0,0 +1,21 @@
+package com.jprogr.taco;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class Ingredient
+{
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type
+    {
+        WRAP,
+        PROTEIN,
+        VEGGIES,
+        CHEESE,
+        SAUCE,
+    }
+}
